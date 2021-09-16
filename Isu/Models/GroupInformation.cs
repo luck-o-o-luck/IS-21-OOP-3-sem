c7c7f27e1572e0369fdfe0fe714c489d4e5d53dc
Ksen <pks2002@yandex.ru>
2021-09-16 11:22:40 +0300

    fix: use Parse

--- Isu/Models/GroupInformation.cs
+++ Isu/Models/GroupInformation.cs
@@ -7,10 +7,10 @@ namespace Isu.Models
     {
         public GroupInformation(string name)
         {
-            Faculty = Convert.ToChar(name.Substring(0, 1));
-            Direction = Convert.ToInt16(name.Substring(1, 1));
-            GroupNumber = Convert.ToInt16(name.Substring(3, 2));
-            CourseNumber = Convert.ToInt16(name.Substring(2, 1));
+            Faculty = char.Parse(name.Substring(0, 1));
+            Direction = short.Parse(name.Substring(1, 1));
+            GroupNumber = short.Parse(name.Substring(3, 2));
+            CourseNumber = short.Parse(name.Substring(2, 1));
 
             if (CourseNumber < 1 || CourseNumber > 4)
                 throw new IsuException("The group can't be created");
