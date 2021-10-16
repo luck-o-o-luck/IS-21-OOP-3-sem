5122045c67e77a2b1128e76f56ddfa258037f451
Ksen <pks2002@yandex.ru>
2021-10-16 22:42:33 +0300

    refactor: rename a variable, add Time for lessons

--- IsuExtra.Tests/IsuExtraServiceTest.cs
+++ IsuExtra.Tests/IsuExtraServiceTest.cs
@@ -25,7 +25,7 @@ namespace IsuExtra.Tests
             
             var group =_isuService.AddGroup("M3201");
             var lessons = new List<Lesson>();
-            lessons.Add(new Lesson("ООП", "08:20", "09:50", "Fredi Cats, Anchous", group, "Tuesday", 151));
+            lessons.Add(new Lesson("ООП", new Time("08:20"), new Time("09:50"), "Fredi Cats, Anchous", group, "Tuesday", 151));
             _isuService.AddScheduleForGroup(new Schedule(lessons), group);
 
             Assert.AreEqual(group.Schedule.Lessons[0].Name, "ООП");
@@ -38,17 +38,17 @@ namespace IsuExtra.Tests
         {
             _isuService = new IsuExtraService();
             
-            var ognp = _isuService.AddOGNP("Прикладная математика и программирование", 'M');
+            var ognp = _isuService.AddOgnp("Прикладная математика и программирование", 'M');
             
             List<Lesson> lessons = new List<Lesson>();
-            lessons.Add(new Lesson("Функциональный анализ", "08:20", "09:50", "Noname", "Monday", 1000));
+            lessons.Add(new Lesson("Функциональный анализ", new Time("08:20"), new Time("09:50"), "Noname", "Monday", 1000));
             var schedule = new Schedule(lessons);
             
-            var streamOGNP = _isuService.AddStreamOGNP(schedule, 'M', "Прикладная математика и программирование");
-            var selectedStreams = _isuService.GetStreamsOGNP(ognp);
+            var streamOGNP = _isuService.AddStreamOgnp(schedule, 'M', "Прикладная математика и программирование");
+            var selectedStreams = _isuService.GetStreamsOgnp(ognp);
             
             Assert.AreEqual(ognp.Streams[0], streamOGNP);
-            Assert.AreEqual(streamOGNP.OGNP, ognp);
+            Assert.AreEqual(streamOGNP.Ognp, ognp);
             Assert.AreEqual(selectedStreams[0], streamOGNP);
         }
 
@@ -60,20 +60,20 @@ namespace IsuExtra.Tests
             var group =_isuService.AddGroup("M3201");
             var lessons = new List<Lesson>();
             var student = _isuService.AddStudent(group, "Васютинская Ксения");
-            lessons.Add(new Lesson("ООП", "08:20", "09:50", "Fredi Cats, Anchous", group, "Tuesday", 151));
+            lessons.Add(new Lesson("ООП", new Time("08:20"), new Time("09:50"), "Fredi Cats, Anchous", group, "Tuesday", 151));
             _isuService.AddScheduleForGroup(new Schedule(lessons), group);
             
-            var ognp = _isuService.AddOGNP("Прикладная математика и программирование", 'M');
+            var ognp = _isuService.AddOgnp("Прикладная математика и программирование", 'M');
             
             List<Lesson> lessonsForOGNP = new List<Lesson>();
-            lessonsForOGNP.Add(new Lesson("Функциональный анализ", "08:20", "09:50", "Noname", "Tuesday", 1000));
+            lessonsForOGNP.Add(new Lesson("Функциональный анализ", new Time("08:20"), new Time("09:50"), "Noname", "Tuesday", 1000));
             var schedule = new Schedule(lessonsForOGNP);
             
-            var streamOGNP = _isuService.AddStreamOGNP(schedule, 'M', "Прикладная математика и программирование");
+            var streamOGNP = _isuService.AddStreamOgnp(schedule, 'M', "Прикладная математика и программирование");
 
             Assert.Catch<IsuExtraException>(()=>
             {
-                _isuService.AddStudentToStreamOGNP(student, streamOGNP);
+                _isuService.AddStudentToStreamOgnp(student, streamOGNP);
             });
         }
 
@@ -82,27 +82,27 @@ namespace IsuExtra.Tests
         {
             _isuService = new IsuExtraService();
             
-            var group =_isuService.AddGroup("M3201");
+            MegafacultyGroup group =_isuService.AddGroup("M3201");
             var lessons = new List<Lesson>();
             var student = _isuService.AddStudent(group, "Васютинская Ксения");
-            lessons.Add(new Lesson("ООП", "08:20", "09:50", "Fredi Cats, Anchous", group, "Tuesday", 151));
+            lessons.Add(new Lesson("ООП", new Time("08:20"), new Time("09:50"), "Fredi Cats, Anchous", group, "Tuesday", 151));
             _isuService.AddScheduleForGroup(new Schedule(lessons), group);
             
-            var ognp = _isuService.AddOGNP("Предпринимательство и инноватика", 'P');
+            var ognp = _isuService.AddOgnp("Предпринимательство и инноватика", 'P');
             
             List<Lesson> lessonsForOGNP = new List<Lesson>();
-            lessonsForOGNP.Add(new Lesson("Экономика приоритетных отраслей", "10:00", "11:30", "Noname", "Tuesday", 1000));
+            lessonsForOGNP.Add(new Lesson("Экономика приоритетных отраслей", new Time("10:00"), new Time("11:30"), "Noname", "Tuesday", 1000));
             var schedule = new Schedule(lessonsForOGNP);
             
-            var streamOGNP = _isuService.AddStreamOGNP(schedule, 'P', "Предпринимательство и инноватика");
-
-            _isuService.AddStudentToStreamOGNP(student, streamOGNP);
+            var streamOGNP = _isuService.AddStreamOgnp(schedule, 'P', "Предпринимательство и инноватика");
 
             var students = _isuService.GetUnsubscribedStudents(group);
             
             Assert.AreEqual(students[0].Name, "Васютинская Ксения");
+            
+            _isuService.AddStudentToStreamOgnp(student, streamOGNP);
 
-            students = _isuService.GetStudentsFromOGNP(streamOGNP);
+            students = _isuService.GetStudentsFromOgnp(streamOGNP);
             
             Assert.AreEqual(students[0].Name, "Васютинская Ксения");
         }
@@ -115,22 +115,21 @@ namespace IsuExtra.Tests
             var group =_isuService.AddGroup("M3201");
             var lessons = new List<Lesson>();
             var student = _isuService.AddStudent(group, "Васютинская Ксения");
-            lessons.Add(new Lesson("ООП", "08:20", "09:50", "Fredi Cats, Anchous", group, "Tuesday", 151));
+            lessons.Add(new Lesson("ООП", new Time("08:20"), new Time("09:50"), "Fredi Cats, Anchous", group, "Tuesday", 151));
             _isuService.AddScheduleForGroup(new Schedule(lessons), group);
             
-            var ognp = _isuService.AddOGNP("Предпринимательство и инноватика", 'P');
+            var ognp = _isuService.AddOgnp("Предпринимательство и инноватика", 'P');
             
             List<Lesson> lessonsForOGNP = new List<Lesson>();
-            lessonsForOGNP.Add(new Lesson("Экономика приоритетных отраслей", "10:00", "11:30", "Noname", "Tuesday", 1000));
+            lessonsForOGNP.Add(new Lesson("Экономика приоритетных отраслей", new Time("10:00"), new Time("11:30"), "Noname", "Tuesday", 1000));
             var schedule = new Schedule(lessonsForOGNP);
             
-            var streamOGNP = _isuService.AddStreamOGNP(schedule, 'P', "Предпринимательство и инноватика");
+            var streamOGNP = _isuService.AddStreamOgnp(schedule, 'P', "Предпринимательство и инноватика");
 
-            _isuService.AddStudentToStreamOGNP(student, streamOGNP);
+            _isuService.AddStudentToStreamOgnp(student, streamOGNP);
 
-            _isuService.CancelStudentsOGNP(student, ognp);
-            Assert.AreEqual(student.CountOGNP, 0);
-            Assert.AreEqual(student.OGNPs.Count, 0);
+            _isuService.CancelStudentsOgnp(student, ognp);
+            Assert.AreEqual(student.Ognp, null);
         }
     }
 }
