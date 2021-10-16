86e27473db1ebe881c589c56b991db3b2aafc018
Ksen <pks2002@yandex.ru>
2021-10-16 22:37:17 +0300

    style: rename OGNP to Ognp

--- IsuExtra/Models/Ognp.cs
+++ IsuExtra/Models/Ognp.cs
@@ -3,25 +3,25 @@ using IsuExtra.Tools;
 
 namespace IsuExtra.Models
 {
-    public class OGNP
+    public class Ognp
     {
-        private List<StreamOGNP> _streams;
+        private List<StreamOgnp> _streams;
 
-        public OGNP(string name, char faculty)
+        public Ognp(string name, char faculty)
         {
             if (string.IsNullOrEmpty(name))
                 throw new IsuExtraException("String is null or empty");
 
             NameOGNP = name;
             Faculty = faculty;
-            _streams = new List<StreamOGNP>();
+            _streams = new List<StreamOgnp>();
         }
 
         public char Faculty { get; }
         public string NameOGNP { get; }
-        public IReadOnlyList<StreamOGNP> Streams => _streams;
+        public IReadOnlyList<StreamOgnp> Streams => _streams;
 
-        public void AddStreamOfOGNP(StreamOGNP stream)
+        public void AddStreamOfOGNP(StreamOgnp stream)
         {
             if (stream is null)
                 throw new IsuExtraException("Stream is null");
