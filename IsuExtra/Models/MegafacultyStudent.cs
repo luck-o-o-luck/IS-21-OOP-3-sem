38061b7065a29309d421c6fb4effde259f2d7b0b
Ksen <pks2002@yandex.ru>
2021-10-16 22:38:40 +0300

    fix: student can add one OGNP

--- IsuExtra/Models/MegafacultyStudent.cs
+++ IsuExtra/Models/MegafacultyStudent.cs
@@ -8,48 +8,40 @@ namespace IsuExtra.Models
     public class MegafacultyStudent : Student
     {
         private const int MaxCountOGNP = 2;
-        private int _countOgnp;
-        private List<StreamOGNP> _ognp;
 
         public MegafacultyStudent(string fullName, MegafacultyGroup group, int id)
-            : base(fullName, group, id)
+            : base(fullName, null, id)
         {
             Group = @group ?? throw new IsuExtraException("OGNP is null");
-            _ognp = new List<StreamOGNP>();
-            _countOgnp = 0;
+            Ognp = null;
         }
 
         public MegafacultyGroup Group { get; }
-        public int CountOGNP => _countOgnp;
-        public IReadOnlyList<StreamOGNP> OGNPs => _ognp;
-        public void AddOGNPForStudent(StreamOGNP ognp)
+        public StreamOgnp Ognp { get; private set; }
+        public void AddOGNPForStudent(StreamOgnp ognp)
         {
             if (ognp is null)
                 throw new IsuExtraException("OGNP is null");
             if (ognp.Faculty == Group.InformationAboutGroup.Faculty)
                 throw new IsuExtraException("You can't choose the course of your faculty");
-            if (_ognp.Count == MaxCountOGNP)
-                throw new IsuExtraException("This student has a two OGNP");
-            if (_countOgnp == MaxCountOGNP)
-                throw new IsuExtraException("This student has max count ognp");
+            if (Ognp != null)
+                throw new IsuExtraException("This student has ognp");
             if (Group.Schedule.Contain(ognp.Schedule.Lessons))
                 throw new IsuExtraException("Intersection of schedules");
 
-            _ognp.Add(ognp);
-            _countOgnp += 1;
+            Ognp = ognp;
         }
 
-        public StreamOGNP CancelOGNP(OGNP ognp)
+        public StreamOgnp CancelOGNP(Ognp ognp)
         {
-            var selectedOGNP = _ognp.SingleOrDefault(x => x.OGNP.NameOGNP == ognp.NameOGNP);
-
-            if (selectedOGNP is null)
+            if (Ognp.Name != ognp.NameOGNP)
+                throw new IsuExtraException("This student hasn't this ognp");
+            if (ognp is null)
                 throw new IsuExtraException("This student doesn't exist this ognp");
 
-            _ognp.Remove(selectedOGNP);
-            _countOgnp -= 1;
+            Ognp = null;
 
-            return selectedOGNP;
+            return Ognp;
         }
     }
 }
