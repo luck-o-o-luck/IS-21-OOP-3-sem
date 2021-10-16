5e14feb9e54a5bb617a9f3f88b84ee141b5d27e8
Ksen <pks2002@yandex.ru>
2021-10-16 22:41:07 +0300

    refactor: use Time for start and end, reusing the constructor

--- IsuExtra/Models/Lesson.cs
+++ IsuExtra/Models/Lesson.cs
@@ -6,61 +6,33 @@ namespace IsuExtra.Models
 {
     public class Lesson
     {
-        public Lesson(string name, string start, string end, string teacher, Group group, string day, int auditorium)
+        public Lesson(string name, Time start, Time end, string teacher, Group<MegafacultyStudent> group, string day, int auditorium)
         {
             if (string.IsNullOrEmpty(name))
                 throw new IsuExtraException("String with name is empty");
-            if (string.IsNullOrEmpty(start))
-                throw new IsuExtraException("String with lessons start is empty");
-            if (string.IsNullOrEmpty(end))
-                throw new IsuExtraException("String with lessons name is empty");
             if (string.IsNullOrEmpty(teacher))
                 throw new IsuExtraException("String with lessons name is empty");
 
             Name = name;
-            StartLessons = new Time(start);
-            EndLessons = new Time(end);
+            StartLessons = start ?? throw new IsuExtraException("String with lessons start is empty");
+            EndLessons = end ?? throw new IsuExtraException("String with lessons name is empty");
             Teacher = teacher;
             Auditorium = auditorium;
-            Group = @group ?? throw new IsuExtraException("The group doesn't exist");
+            Group = group;
             Day = day;
         }
 
-        public Lesson(string name, string start, string end, string teacher, string day, int auditorium)
+        public Lesson(string name, Time start, Time end, string teacher, string day, int auditorium)
+            : this(name, start, end, teacher, null, day, auditorium)
         {
-            if (string.IsNullOrEmpty(name))
-                throw new IsuExtraException("String with name is empty");
-            if (string.IsNullOrEmpty(start))
-                throw new IsuExtraException("String with lessons start is empty");
-            if (string.IsNullOrEmpty(end))
-                throw new IsuExtraException("String with lessons name is empty");
-            if (string.IsNullOrEmpty(teacher))
-                throw new IsuExtraException("String with lessons name is empty");
-            if (auditorium < 1)
-                throw new IsuExtraException("This auditorium goesn't exist");
-
-            Name = name;
-            StartLessons = new Time(start);
-            EndLessons = new Time(end);
-            Teacher = teacher;
-            Auditorium = auditorium;
-            Day = day;
         }
 
         public string Name { get; }
         public string Teacher { get; }
-        public Group Group { get; }
+        public Group<MegafacultyStudent> Group { get; }
         public int Auditorium { get; }
         public Time StartLessons { get; private set; }
         public Time EndLessons { get; private set; }
         public string Day { get; }
-
-        public Lesson ChangeTimeLessons(Lesson lesson, string start, string end)
-        {
-            lesson.StartLessons = new Time(start);
-            lesson.EndLessons = new Time(end);
-
-            return lesson;
-        }
     }
 }
