deedc30b8621bc1ce7a737e0d6d67b2154fda09b
Ksen <pks2002@yandex.ru>
2022-01-05 18:12:46 +0300

    style: change checks and rename Task to WorkTask

--- ReportsDomain/Models/Employee.cs
+++ ReportsDomain/Models/Employee.cs
@@ -9,13 +9,13 @@ namespace ReportsDomain.Models
     public class Employee
     {
         private List<Employee> _subordinates;
-        private List<Task> _tasks;
+        private List<WorkTask> _tasks;
         
         public Employee() {}
         public Employee(NameEmployee name, EmployeeStatus status)
         {
             _subordinates = new List<Employee>();
-            _tasks = new List<Task>();
+            _tasks = new List<WorkTask>();
             Name = name ?? throw new ReportsException("Name is null");
             Status = status;
             Id = Guid.NewGuid();
@@ -25,40 +25,34 @@ namespace ReportsDomain.Models
         public Guid Id { get; set; } 
         public EmployeeStatus Status { get; set; }
         public Employee Chief { get; set; }
-        public IEnumerable<object> Tasks { get; set; }
+        public IReadOnlyList<WorkTask> Tasks => _tasks;
 
-        public bool ExistsTask(Task task) => _tasks.Any(x => x.Id == task.Id);
+        public bool ExistsTask(WorkTask workTask) => _tasks.Any(x => x.Id == workTask.Id);
         public bool ExistsSubordinate(Employee employee) => _subordinates.Any(x => x.Id == employee.Id);
 
         public void SetChief(Employee employee)
         {
-            if (employee.Status == Status)
-                throw new ReportsException("Can't be the chief");
-            if (employee.Status == EmployeeStatus.OrdinaryEmployee)
-                throw new ReportsException("Can't be the chief");
-            if (employee.Status == EmployeeStatus.Supervisor && Status == EmployeeStatus.TeamLead)
+            if (employee.Status <= Status)
                 throw new ReportsException("Can't be the chief");
 
             Chief = employee;
         }
 
-        public void AddTask(Task task)
+        public void AddTask(WorkTask workTask)
         {
-            if (ExistsTask(task))
+            if (ExistsTask(workTask))
                 throw new ReportsException("Task already exists");
             
-            _tasks.Add(task);
+            _tasks.Add(workTask);
         }
 
         public void AddSubordinates(Employee employee)
         {
             if (ExistsSubordinate(employee))
                 throw new ReportsException("Subordinate already exist");
-            if (employee.Status == EmployeeStatus.TeamLead)
+            if (employee.Status >= Status)
                 throw new ReportsException("Can't be the subordinate");
-            if (employee.Status == EmployeeStatus.Supervisor && Status != EmployeeStatus.TeamLead)
-                throw new ReportsException("Can't be the subordinate");
-            
+
             _subordinates.Add(employee);
         }
     }
